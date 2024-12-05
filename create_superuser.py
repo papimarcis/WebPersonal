6019180caa13de61fcb90e74f0888b558e2d1c1a
Marcos Grande <44397087+papimarcis@users.noreply.github.com>
2024-12-05 18:43:55 +0100

    Agregar verificación para la creación del superusuario y mensajes de estado

--- create_superuser.py
+++ create_superuser.py
@@ -7,4 +7,8 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-User.objects.create_superuser('admin', 'admin@admin.com', os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin'))
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@admin.com', os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin'))
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
